09b23ad080f102d227ab4e0afcebe998eb6bb899
oskov <faklosg@gmail.com>
2020-03-07 14:18:52 +0200

    Add comments

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+#git
 sudo apt install git
 git config --global user.name "oskov"
 git config --global user.email "faklosg@gmail.com"
+#stuff
 sudo apt-get update
 sudo apt-get install \apt-transport-https \ ca-certificates \ curl \ gnupg-agent \ software-properties-common
 sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg-agent     software-properties-common
+#docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -13,6 +16,7 @@ sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/u
 sudo apt-get update 
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo docker run hello-world
+#golang
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt-get update 
 sudo apt-get install golang-go

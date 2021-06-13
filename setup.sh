1022a98c99a0c19967484d6092fb8a955c9dd0f2
oskov <faklosg@gmail.com>
2021-06-13 16:15:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,20 +3,13 @@
 sudo apt install git
 git config --global user.name "oskov"
 git config --global user.email "faklosg@gmail.com"
-#stuff
-sudo apt update
-sudo apt install \apt-transport-https \ ca-certificates \ curl \ gnupg-agent \ software-properties-common
-sudo apt install     apt-transport-https     ca-certificates     curl     gnupg-agent     software-properties-common
-#docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt update 
-sudo apt install docker-ce docker-ce-cli containerd.io
-sudo docker run hello-world
-#golang
-sudo add-apt-repository ppa:longsleep/golang-backports
-sudo apt update 
-sudo apt install golang-go
+
+pamac install nvm
+
+pamac install docker
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+sudo usermod -aG docker $USER
+pamac install docker-compose
+
+ssh-keygen
